923a3ea9ab6818872bb39d242d8939dbb41403f2
hnifan <FenixSatan@outlook.com>
2025-11-06 16:50:36 +0800

    Performance Second Version

---
+++ core/src/main/java/com/taobao/arthas/core/command/model/PerformanceData.java
@@ -0,0 +1,190 @@
+package com.taobao.arthas.core.command.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 性能数据辅助类
+ * 用于存储和传递性能指标数据
+ *
+ * @author Huang Junhao
+ */
+public class PerformanceData {
+    
+    private long totalMemory;
+    private long freeMemory;
+    private long maxMemory;
+    private long usedMemory;
+    private double memoryUsagePercent;
+    
+    private int threadCount;
+    private int peakThreadCount;
+    private int daemonThreadCount;
+    
+    private long gcCount;
+    private long gcTime;
+    
+    private long loadedClassCount;
+    private long totalLoadedClassCount;
+    private long unloadedClassCount;
+    
+    private int availableProcessors;
+    
+    private int performanceScore;
+    private String performanceLevel;
+    private List<String> issues = new ArrayList<>();
+    private List<String> recommendations = new ArrayList<>();
+    private List<String> alerts = new ArrayList<>();
+
+    public long getTotalMemory() {
+        return totalMemory;
+    }
+
+    public void setTotalMemory(long totalMemory) {
+        this.totalMemory = totalMemory;
+    }
+
+    public long getFreeMemory() {
+        return freeMemory;
+    }
+
+    public void setFreeMemory(long freeMemory) {
+        this.freeMemory = freeMemory;
+    }
+
+    public long getMaxMemory() {
+        return maxMemory;
+    }
+
+    public void setMaxMemory(long maxMemory) {
+        this.maxMemory = maxMemory;
+    }
+
+    public long getUsedMemory() {
+        return usedMemory;
+    }
+
+    public void setUsedMemory(long usedMemory) {
+        this.usedMemory = usedMemory;
+    }
+
+    public double getMemoryUsagePercent() {
+        return memoryUsagePercent;
+    }
+
+    public void setMemoryUsagePercent(double memoryUsagePercent) {
+        this.memoryUsagePercent = memoryUsagePercent;
+    }
+
+    public int getThreadCount() {
+        return threadCount;
+    }
+
+    public void setThreadCount(int threadCount) {
+        this.threadCount = threadCount;
+    }
+
+    public int getPeakThreadCount() {
+        return peakThreadCount;
+    }
+
+    public void setPeakThreadCount(int peakThreadCount) {
+        this.peakThreadCount = peakThreadCount;
+    }
+
+    public int getDaemonThreadCount() {
+        return daemonThreadCount;
+    }
+
+    public void setDaemonThreadCount(int daemonThreadCount) {
+        this.daemonThreadCount = daemonThreadCount;
+    }
+
+    public long getGcCount() {
+        return gcCount;
+    }
+
+    public void setGcCount(long gcCount) {
+        this.gcCount = gcCount;
+    }
+
+    public long getGcTime() {
+        return gcTime;
+    }
+
+    public void setGcTime(long gcTime) {
+        this.gcTime = gcTime;
+    }
+
+    public long getLoadedClassCount() {
+        return loadedClassCount;
+    }
+
+    public void setLoadedClassCount(long loadedClassCount) {
+        this.loadedClassCount = loadedClassCount;
+    }
+
+    public long getTotalLoadedClassCount() {
+        return totalLoadedClassCount;
+    }
+
+    public void setTotalLoadedClassCount(long totalLoadedClassCount) {
+        this.totalLoadedClassCount = totalLoadedClassCount;
+    }
+
+    public long getUnloadedClassCount() {
+        return unloadedClassCount;
+    }
+
+    public void setUnloadedClassCount(long unloadedClassCount) {
+        this.unloadedClassCount = unloadedClassCount;
+    }
+
+    public int getAvailableProcessors() {
+        return availableProcessors;
+    }
+
+    public void setAvailableProcessors(int availableProcessors) {
+        this.availableProcessors = availableProcessors;
+    }
+
+    public int getPerformanceScore() {
+        return performanceScore;
+    }
+
+    public void setPerformanceScore(int performanceScore) {
+        this.performanceScore = performanceScore;
+    }
+
+    public String getPerformanceLevel() {
+        return performanceLevel;
+    }
+
+    public void setPerformanceLevel(String performanceLevel) {
+        this.performanceLevel = performanceLevel;
+    }
+
+    public List<String> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(List<String> issues) {
+        this.issues = issues;
+    }
+
+    public List<String> getRecommendations() {
+        return recommendations;
+    }
+
+    public void setRecommendations(List<String> recommendations) {
+        this.recommendations = recommendations;
+    }
+
+    public List<String> getAlerts() {
+        return alerts;
+    }
+
+    public void setAlerts(List<String> alerts) {
+        this.alerts = alerts;
+    }
+}
